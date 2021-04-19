d558f5f21e285cda5e75134989b37ab623d033b9
Satyamkumarai <satyamkumarai@gmail.com>
2021-04-19 15:45:05 +0530

    Update README

--- mapping/mapping_using_serial/main/helpers/cmd_interface.py
+++ mapping/mapping_using_serial/main/helpers/cmd_interface.py
@@ -7,7 +7,7 @@ Usage Examples:
         python main.py -f 'com4' -s -b 9600
         
     Read from COM4 using baudrate 9600 and save to file data/output.csv:
-        python main.py -f 'com4' -s -b 9600
+        python main.py -f 'com4' -s -b 9600 -o data/output.csv
         
     Read from a file: 
         python main.py -f <filename> 
